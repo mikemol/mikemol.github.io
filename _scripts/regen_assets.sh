a5e293ebe9854e64be48d3c55a9df797ff1e77a8
Mike Mol <mikemol@gmail.com>
2018-02-11 20:28:34 -0500

    Support protected branches

--- _scripts/regen_assets.sh
+++ _scripts/regen_assets.sh
@@ -1,23 +1,33 @@
-#!/bin/bash
-fmts="svg png"
+    #!/bin/bash
+    fmts="svg png"
+    banned_branches="master regen-assets"
 
-while IFS= read -r -d '' dotfile ; do
-    assetdir=$(dirname "${dotfile}")
-    assetname=$(basename "${assetdir}")
-    echo "Generating renders for $(basename ${assetdir})"
-    for fmt in ${fmts} ; do
-        dstfile="${assetdir}/${assetname}.${fmt}"
-        if [[ "${dotfile}" -nt "${dstfile}" ]] ; then
-            echo "Building ${fmt} for $(basename ${assetdir})"
-            dot "-T${fmt}" -o "${assetdir}/${assetname}.${fmt}" "${dotfile}"
-            if [[ $fmt == "png" ]] ; then
-                orig_size=$(stat -c %s "${dstfile}")
-                pngquant --force --ext .png -Q 100-100 "${dstfile}"
-                quant_size=$(stat -c %s "${dstfile}")
-                pngcrush -ow -brute -l 9 -q "${dstfile}"
-                crushed_size=$(stat -c %s "${dstfile}")
-                echo "Original: ${orig_size}b. Quantized: ${quant_size}b. Crushed: ${crushed_size}"
-            fi
+    current_branch=$(git branch -l | grep -e '^*' | cut -d' ' -f2)
+
+    for b in ${banned_branches} ; do
+        if [[ "${current_branch}" == "${b}" ]]; then
+            echo "Not regenerating assets. ${b} is a protected branch."
+            exit 1
         fi
     done
-done < <(find assets -type f -name '*.dot' -print0)
+
+    while IFS= read -r -d '' dotfile ; do
+        assetdir=$(dirname "${dotfile}")
+        assetname=$(basename "${assetdir}")
+        echo "Generating renders for $(basename ${assetdir})"
+        for fmt in ${fmts} ; do
+            dstfile="${assetdir}/${assetname}.${fmt}"
+            if [[ "${dotfile}" -nt "${dstfile}" ]] ; then
+                echo "Building ${fmt} for $(basename ${assetdir})"
+                dot "-T${fmt}" -o "${assetdir}/${assetname}.${fmt}" "${dotfile}"
+                if [[ $fmt == "png" ]] ; then
+                    orig_size=$(stat -c %s "${dstfile}")
+                    pngquant --force --ext .png -Q 100-100 "${dstfile}"
+                    quant_size=$(stat -c %s "${dstfile}")
+                    pngcrush -ow -brute -l 9 -q "${dstfile}"
+                    crushed_size=$(stat -c %s "${dstfile}")
+                    echo "Original: ${orig_size}b. Quantized: ${quant_size}b. Crushed: ${crushed_size}"
+                fi
+            fi
+        done
+    done < <(find assets -type f -name '*.dot' -print0)
